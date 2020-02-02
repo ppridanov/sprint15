0548ae0c0d9e1cb590d93fd903be6870d45c8cde
unknown <Petr Pridanov>
2020-02-02 08:05:46 +0600

    change route adress for getuser

--- routes/users.js
+++ routes/users.js
@@ -16,7 +16,7 @@ const {
 } = require('../controllers/users');
 
 router.get('/users', auth, getAllUsers);
-router.get('/users/:userId', auth, getUser);
+router.get('/users/me', auth, getUser);
 router.post('/signup', newUserValidator, createUser);
 router.patch('/users/me', auth, updateUserinfoValidator, updateUserInfo);
 router.patch('/users/me/avatar', auth, updateUserAvatarValidator, updateUserAvatar);
